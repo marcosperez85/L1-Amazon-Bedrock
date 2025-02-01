51f63d6368dbae36e18702036d38225226ec571b
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-02-01 20:38:17 -0300

    Borré la importación de la librería de audio ya que no se va a usar aun. También dejé comentada la invocación al modelo para evitar que haga llamados a AWS por cada prueba que necesite hacer con la transcripción (que es lo que nos interesa)

--- L1_Your_first_generations_with_Amazon_Bedrock.py
+++ L1_Your_first_generations_with_Amazon_Bedrock.py
@@ -14,7 +14,6 @@
 # ### Import all needed packages
 import boto3
 import json
-#from IPython.display import Audio
 
 session = boto3.Session(profile_name = "AdministratorAccess-376129873205")
 
@@ -39,8 +38,9 @@ kwargs = {
 # La razón por la que se pone **kwargs con los asteriscos es porque los argumentos de la función "invoke_model" van separados por comas.
 # Por lo tanto podríamos poner cada uno de los parámetros (modelId, contentType, etc) en forma individual pero sería difícil de leer.
 # En cambio con **kwargs, la función sabe que no se le está pasando un JSON sino que tiene que tomar los elementos que tiene dentro.
+"""
 response = bedrock_runtime.invoke_model(**kwargs)
-response_body = json.loads(response.get('body').read())
+response_body = json.loads(response.get('body').read())"""
 
 # La variable "response" contiene un JSON con metadatos pero no tiene metadatos pero no tiene el resultado que nos da el LLM.
 # Así es como luce: 'body': <botocore.response.StreamingBody at 0x7f282847b3a0>}
@@ -48,7 +48,7 @@ response_body = json.loads(response.get('body').read())
 # Eso nos da otro JSON con sus propios key:values entre los cuales está el resultado del LLM
 
 # print(json.dumps(response_body, indent=4))
-print(response_body['results'][0]['outputText'])
+# print(response_body['results'][0]['outputText'])
 
 # Al ejecutar eso vemos que el "response_body" tiene la respuesta del LLM en el key llamado "outputText" por lo que vamos a 
 # mostrar en la terminal sólo eso: el outputText del elemento 0 del key "results"
@@ -72,12 +72,13 @@ kwargs = {
     )
 }
 
+"""
 response2 = bedrock_runtime.invoke_model(**kwargs)
 response2_body = json.loads(response2.get('body').read())
 
 # print(json.dumps(response_body, indent=4))
 generation = response2_body['results'][0]['outputText']
-print(generation)
+print(generation)"""
 
 # El parámetro "maxTokenCount" de 100 es para limitar el número de tokens que nos va a devolver el LLM.
 # No es el número exacto que vamos a obtener sino el máximo que le permitimos que use en la respuesta.
@@ -90,6 +91,12 @@ print(generation)
 # tokens antes de terminar la generación. Eso se puede ver mostrando en pantalla el response_body completo donde se
 # ve el "completionReason". Si llega a decir LENGTH es porque se acabaron los tokens y hace falta aumentarlos o reducir 
 # el temperature. Esto último es porque un mayor número de tokens implica un costo mayor por el uso del LLM.
+
+
+# ### Working with other type of data
+# En esta sección el instructor muestra cómo reproducir un archivo de audio pero es sólo eso, muestra cómo reproducirlo 
+# sin realizarle ningún procesamiento. Es decir, aun no vamos a ver cómo obtener una transcripción sino que brinda una
+# transcripción ya hecha para luego crear un resumen mediante ingeniería de prompt. 
 
 
 
